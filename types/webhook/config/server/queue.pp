1d83e37716ee485df306542e239027536e9bece5
Elfranne <861038+elfranne@users.noreply.github.com>
2025-05-01 15:47:37 +0200

    Fix data type

--- types/webhook/config/server/queue.pp
+++ types/webhook/config/server/queue.pp
@@ -1,6 +1,6 @@
 # @summary webhook config server queue type
 type R10k::Webhook::Config::Server::Queue = Struct[{
     enabled             => Boolean,
-    max_concurrent_jobs => Optional[Int],
-    max_history_items   => Optional[Int],
+    max_concurrent_jobs => Optional[Integer],
+    max_history_items   => Optional[Integer],
 }]
